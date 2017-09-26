43a1371b42b27b07f90b011f33be7a69f3729eaf
jmkjohns <jmkjohns@mac.com>
2017-09-26 18:40:00 +0100

    Dealt with edge cases in sumProperDivisors

--- Common/Divisors.cpp
+++ Common/Divisors.cpp
@@ -10,24 +10,28 @@ unsigned int sumProperDivisors( unsigned int number )
 //==============================================================================
 {
     //declare 
-    unsigned int total = 1;
-    unsigned int limit = intRoot( number );
+    unsigned int total = 0;
 
-    for (unsigned int i = 2; i < limit; ++i)
+    if (number > 1)
     {
-        if ( number % i == 0)
+        unsigned int limit = intRoot( number );
+        total = 1;
+
+        for (unsigned int i = 2; i < limit; ++i)
         {
-            total += i;
-            total += (number / i);
+            if ( number % i == 0)
+            {
+                total += i;
+                total += (number / i);
+            }
+        }
+
+        //deal with perfect squares
+        if (limit * limit == number)
+        {
+            total += limit;
         }
     }
-
-    //deal with perfect squares
-    if (limit * limit == number)
-    {
-        total += limit;
-    }
-
     return total;
 }
 
